b99d48203acfb2675d839fbbbd8595aef1835812
zfletch <zfletch2@gmail.com>
2020-11-16 12:00:52 -0500

    update how xml is loaded

---
+++ src/demo/App/alignment.js
@@ -0,0 +1,525 @@
+export default `
+<aligned-text xmlns="http://alpheios.net/namespaces/aligned-text">
+  <language lnum="L1" xml:lang="grc" dir="ltr"/>
+  <language lnum="L2" xml:lang="eng" dir="ltr"/>
+  <comment class="title">Iliad 1.1-7</comment>
+  <sentence id="1" document_id="">
+    <wds lnum="L1">
+      <comment class="uri"/>
+      <w n="1-1" id="1">
+        <text>μῆνιν</text>
+        <refs nrefs="1-1 1-2"/>
+      </w>
+      <w n="1-2" id="2">
+        <text>ἄειδε</text>
+        <refs nrefs="1-3 1-7"/>
+      </w>
+      <w n="1-3" id="3">
+        <text>θεὰ</text>
+        <refs nrefs="1-5"/>
+      </w>
+      <w n="1-4" id="4">
+        <text>Πηληϊάδεω</text>
+        <refs nrefs="1-8 1-9 1-10"/>
+      </w>
+      <w n="1-5" id="5">
+        <text>Ἀχιλῆος</text>
+        <refs nrefs="1-12"/>
+      </w>
+      <w n="" id="5-1">
+        <text>
+</text>
+      </w>
+      <w n="1-6" id="6">
+        <text>οὐλομένην</text>
+        <refs nrefs="1-14 1-15 1-16"/>
+      </w>
+      <w n="1-7" id="7">
+        <text>,</text>
+      </w>
+      <w n="1-8" id="8">
+        <text>ἣ</text>
+        <refs nrefs="1-17"/>
+      </w>
+      <w n="1-9" id="9">
+        <text>μυρί᾽</text>
+        <refs nrefs="1-19"/>
+      </w>
+      <w n="1-10" id="10">
+        <text>Ἀχαιοῖς</text>
+        <refs nrefs="1-21 1-22 1-23"/>
+      </w>
+      <w n="1-11" id="11">
+        <text>ἄλγε᾽</text>
+        <refs nrefs="1-20"/>
+      </w>
+      <w n="1-12" id="12">
+        <text>ἔθηκε</text>
+        <refs nrefs="1-18"/>
+      </w>
+      <w n="1-13" id="13">
+        <text>,</text>
+      </w>
+      <w n="" id="12-1">
+        <text>
+</text>
+      </w>
+      <w n="1-14" id="14">
+        <text>πολλὰς</text>
+        <refs nrefs="1-30"/>
+      </w>
+      <w n="1-15" id="15">
+        <text>δ᾽</text>
+        <refs nrefs="1-25"/>
+      </w>
+      <w n="1-16" id="16">
+        <text>ἰφθίμους</text>
+        <refs nrefs="1-31"/>
+      </w>
+      <w n="1-17" id="17">
+        <text>ψυχὰς</text>
+        <refs nrefs="1-32"/>
+      </w>
+      <w n="1-18" id="18">
+        <text>Ἄϊδι</text>
+        <refs nrefs="1-28 1-29"/>
+      </w>
+      <w n="1-19" id="19">
+        <text>προΐαψεν</text>
+        <refs nrefs="1-26 1-27"/>
+      </w>
+      <w n="" id="19-1">
+        <text>
+</text>
+      </w>
+      <w n="1-20" id="20">
+        <text>ἡρώων</text>
+        <refs nrefs="1-33 1-34"/>
+      </w>
+      <w n="1-21" id="21">
+        <text>,</text>
+      </w>
+      <w n="1-22" id="22">
+        <text>αὐτοὺς</text>
+        <refs nrefs="1-38 1-39"/>
+      </w>
+      <w n="1-23" id="23">
+        <text>δὲ</text>
+        <refs nrefs="1-36"/>
+      </w>
+      <w n="1-24" id="24">
+        <text>ἑλώρια</text>
+        <refs nrefs="1-40"/>
+      </w>
+      <w n="1-25" id="25">
+        <text>τεῦχε</text>
+        <refs nrefs="1-37"/>
+      </w>
+      <w n="1-26" id="26">
+        <text>κύνεσσιν</text>
+        <refs nrefs="1-41 1-42"/>
+      </w>
+      <w n="" id="26-1">
+        <text>
+</text>
+      </w>
+      <w n="1-27" id="27">
+        <text>οἰωνοῖσί</text>
+        <refs nrefs="1-45"/>
+      </w>
+      <w n="1-28" id="28">
+        <text>τε</text>
+        <refs nrefs="1-43"/>
+      </w>
+      <w n="1-29" id="29">
+        <text>πᾶσι</text>
+        <refs nrefs="1-44"/>
+      </w>
+      <w n="1-30" id="30">
+        <text>,</text>
+      </w>
+      <w n="1-31" id="31">
+        <text>Διὸς</text>
+        <refs nrefs="2-4 2-5"/>
+      </w>
+      <w n="1-32" id="32">
+        <text>δ᾽</text>
+        <refs nrefs="2-1"/>
+      </w>
+      <w n="1-33" id="33">
+        <text>ἐτελείετο</text>
+        <refs nrefs="2-6 2-7 2-8"/>
+      </w>
+      <w n="1-34" id="34">
+        <text>βουλή</text>
+        <refs nrefs="2-2 2-3"/>
+      </w>
+      <w n="1-35" id="35">
+        <text>,</text>
+      </w>
+      <w n="" id="34-1">
+        <text>
+</text>
+      </w>
+      <w n="1-36" id="36">
+        <text>ἐξ</text>
+        <refs nrefs="2-10"/>
+      </w>
+      <w n="1-37" id="37">
+        <text>οὗ</text>
+        <refs nrefs="2-11 2-12 2-13"/>
+      </w>
+      <w n="1-38" id="38">
+        <text>δὴ</text>
+      </w>
+      <w n="1-39" id="39">
+        <text>τὰ</text>
+        <refs nrefs="2-14"/>
+      </w>
+      <w n="1-40" id="40">
+        <text>πρῶτα</text>
+        <refs nrefs="2-14"/>
+      </w>
+      <w n="1-41" id="41">
+        <text>διαστήτην</text>
+        <refs nrefs="2-15 2-16"/>
+      </w>
+      <w n="1-42" id="42">
+        <text>ἐρίσαντε</text>
+        <refs nrefs="2-17 2-18"/>
+      </w>
+      <w n="" id="42-1">
+        <text>
+</text>
+      </w>
+      <w n="1-43" id="43">
+        <text>Ἀτρεΐδης</text>
+        <refs nrefs="2-19 2-20 2-21"/>
+      </w>
+      <w n="1-44" id="44">
+        <text>τε</text>
+        <refs nrefs="2-27"/>
+      </w>
+      <w n="1-45" id="45">
+        <text>ἄναξ</text>
+        <refs nrefs="2-23"/>
+      </w>
+      <w n="1-46" id="46">
+        <text>ἀνδρῶν</text>
+        <refs nrefs="2-24 2-25"/>
+      </w>
+      <w n="1-47" id="47">
+        <text>καὶ</text>
+        <refs nrefs="2-27"/>
+      </w>
+      <w n="1-48" id="48">
+        <text>δῖος</text>
+        <refs nrefs="2-28"/>
+      </w>
+      <w n="1-49" id="49">
+        <text>Ἀχιλλεύς</text>
+        <refs nrefs="2-29"/>
+      </w>
+      <w n="1-50" id="50">
+        <text>.</text>
+      </w>
+    </wds>
+    <wds lnum="L2">
+      <comment class="uri"/>
+      <w n="1-1">
+        <text>The</text>
+        <refs nrefs="1-1"/>
+      </w>
+      <w n="1-2">
+        <text>wrath</text>
+        <refs nrefs="1-1"/>
+      </w>
+      <w n="1-3">
+        <text>sing</text>
+        <refs nrefs="1-2"/>
+      </w>
+      <w n="1-4">
+        <text>,</text>
+      </w>
+      <w n="1-5">
+        <text>goddess</text>
+        <refs nrefs="1-3"/>
+      </w>
+      <w n="1-6">
+        <text>,</text>
+      </w>
+      <w n="1-7">
+        <text>of</text>
+        <refs nrefs="1-2"/>
+      </w>
+      <w n="1-8">
+        <text>Peleus</text>
+        <refs nrefs="1-4"/>
+      </w>
+      <w n="1-9">
+        <text>'</text>
+        <refs nrefs="1-4"/>
+      </w>
+      <w n="1-10">
+        <text>son</text>
+        <refs nrefs="1-4"/>
+      </w>
+      <w n="1-11">
+        <text>,</text>
+      </w>
+      <w n="1-12">
+        <text>Achilles</text>
+        <refs nrefs="1-5"/>
+      </w>
+      <w n="1-13">
+        <text>,</text>
+      </w>
+      <w n="1-14">
+        <text>that</text>
+        <refs nrefs="1-6"/>
+      </w>
+      <w n="1-15">
+        <text>destructive</text>
+        <refs nrefs="1-6"/>
+      </w>
+      <w n="1-16">
+        <text>wrath</text>
+        <refs nrefs="1-6"/>
+      </w>
+      <w n="1-17">
+        <text>which</text>
+        <refs nrefs="1-8"/>
+      </w>
+      <w n="1-18">
+        <text>brought</text>
+        <refs nrefs="1-12"/>
+      </w>
+      <w n="1-19">
+        <text>countless</text>
+        <refs nrefs="1-9"/>
+      </w>
+      <w n="1-20">
+        <text>woes</text>
+        <refs nrefs="1-11"/>
+      </w>
+      <w n="1-21">
+        <text>upon</text>
+        <refs nrefs="1-10"/>
+      </w>
+      <w n="1-22">
+        <text>the</text>
+        <refs nrefs="1-10"/>
+      </w>
+      <w n="1-23">
+        <text>Achaeans</text>
+        <refs nrefs="1-10"/>
+      </w>
+      <w n="1-24">
+        <text>,</text>
+      </w>
+      <w n="1-25">
+        <text>and</text>
+        <refs nrefs="1-15"/>
+      </w>
+      <w n="1-26">
+        <text>sent</text>
+        <refs nrefs="1-19"/>
+      </w>
+      <w n="1-27">
+        <text>forth</text>
+        <refs nrefs="1-19"/>
+      </w>
+      <w n="1-28">
+        <text>to</text>
+        <refs nrefs="1-18"/>
+      </w>
+      <w n="1-29">
+        <text>Hades</text>
+        <refs nrefs="1-18"/>
+      </w>
+      <w n="1-30">
+        <text>many</text>
+        <refs nrefs="1-14"/>
+      </w>
+      <w n="1-31">
+        <text>valiant</text>
+        <refs nrefs="1-16"/>
+      </w>
+      <w n="1-32">
+        <text>souls</text>
+        <refs nrefs="1-17"/>
+      </w>
+      <w n="1-33">
+        <text>of</text>
+        <refs nrefs="1-20"/>
+      </w>
+      <w n="1-34">
+        <text>heroes</text>
+        <refs nrefs="1-20"/>
+      </w>
+      <w n="1-35">
+        <text>,</text>
+      </w>
+      <w n="1-36">
+        <text>and</text>
+        <refs nrefs="1-23"/>
+      </w>
+      <w n="1-37">
+        <text>made</text>
+        <refs nrefs="1-25"/>
+      </w>
+      <w n="1-38">
+        <text>them</text>
+        <refs nrefs="1-22"/>
+      </w>
+      <w n="1-39">
+        <text>themselves</text>
+        <refs nrefs="1-22"/>
+      </w>
+      <w n="1-40">
+        <text>spoil</text>
+        <refs nrefs="1-24"/>
+      </w>
+      <w n="1-41">
+        <text>for</text>
+        <refs nrefs="1-26"/>
+      </w>
+      <w n="1-42">
+        <text>dogs</text>
+        <refs nrefs="1-26"/>
+      </w>
+      <w n="1-43">
+        <text>and</text>
+        <refs nrefs="1-28"/>
+      </w>
+      <w n="1-44">
+        <text>every</text>
+        <refs nrefs="1-29"/>
+      </w>
+      <w n="1-45">
+        <text>bird</text>
+        <refs nrefs="1-27"/>
+      </w>
+      <w n="1-46">
+        <text>;</text>
+      </w>
+      <w n="2-1">
+        <text>thus</text>
+        <refs nrefs="1-32"/>
+      </w>
+      <w n="2-2">
+        <text>the</text>
+        <refs nrefs="1-34"/>
+      </w>
+      <w n="2-3">
+        <text>plan</text>
+        <refs nrefs="1-34"/>
+      </w>
+      <w n="2-4">
+        <text>of</text>
+        <refs nrefs="1-31"/>
+      </w>
+      <w n="2-5">
+        <text>Zeus</text>
+        <refs nrefs="1-31"/>
+      </w>
+      <w n="2-6">
+        <text>came</text>
+        <refs nrefs="1-33"/>
+      </w>
+      <w n="2-7">
+        <text>to</text>
+        <refs nrefs="1-33"/>
+      </w>
+      <w n="2-8">
+        <text>fulfillment</text>
+        <refs nrefs="1-33"/>
+      </w>
+      <w n="2-9">
+        <text>,</text>
+      </w>
+      <w n="2-10">
+        <text>from</text>
+        <refs nrefs="1-36"/>
+      </w>
+      <w n="2-11">
+        <text>the</text>
+        <refs nrefs="1-37"/>
+      </w>
+      <w n="2-12">
+        <text>time</text>
+        <refs nrefs="1-37"/>
+      </w>
+      <w n="2-13">
+        <text>when</text>
+        <refs nrefs="1-37"/>
+      </w>
+      <w n="2-14">
+        <text>first</text>
+        <refs nrefs="1-40"/>
+      </w>
+      <w n="2-15">
+        <text>they</text>
+        <refs nrefs="1-41"/>
+      </w>
+      <w n="2-16">
+        <text>parted</text>
+        <refs nrefs="1-41"/>
+      </w>
+      <w n="2-17">
+        <text>in</text>
+        <refs nrefs="1-42"/>
+      </w>
+      <w n="2-18">
+        <text>strife</text>
+        <refs nrefs="1-42"/>
+      </w>
+      <w n="2-19">
+        <text>Atreus</text>
+        <refs nrefs="1-43"/>
+      </w>
+      <w n="2-20">
+        <text>'</text>
+        <refs nrefs="1-43"/>
+      </w>
+      <w n="2-21">
+        <text>son</text>
+        <refs nrefs="1-43"/>
+      </w>
+      <w n="2-22">
+        <text>,</text>
+      </w>
+      <w n="2-23">
+        <text>king</text>
+        <refs nrefs="1-45"/>
+      </w>
+      <w n="2-24">
+        <text>of</text>
+        <refs nrefs="1-46"/>
+      </w>
+      <w n="2-25">
+        <text>men</text>
+        <refs nrefs="1-46"/>
+      </w>
+      <w n="2-26">
+        <text>,</text>
+      </w>
+      <w n="2-27">
+        <text>and</text>
+        <refs nrefs="1-47"/>
+      </w>
+      <w n="2-28">
+        <text>brilliant</text>
+        <refs nrefs="1-48"/>
+      </w>
+      <w n="2-29">
+        <text>Achilles</text>
+        <refs nrefs="1-49"/>
+      </w>
+      <w n="2-30">
+        <text>.</text>
+      </w>
+    </wds>
+  </sentence>
+ </aligned-text>
+`
---
+++ src/demo/App/treebank.js
@@ -0,0 +1,77 @@
+export default `
+<treebank xml:lang="grc" format="aldt" version="1.5">
+   <date>Thu Aug 21 19:31:06 +0000 2014</date>
+   <annotator>
+     <short/>
+     <name/>
+     <address/>
+     <uri>http://services.perseids.org/llt/segtok</uri>
+   </annotator>
+   <annotator>
+     <short/>
+     <name/>
+     <address/>
+     <uri>http://github.com/latin-language-toolkit/arethusa</uri>
+   </annotator>
+   <annotator>
+     <short>Vanessa Gorman</short>
+     <name>Vanessa Gorman</name>
+     <address>vbgorman@gmail.com</address>
+     <uri>
+       http://data.perseus.org/sosol/users/Vanessa%20Gorman
+     </uri>
+   </annotator>
+      <sentence id="1" subdoc="1.1-1.7" document_id="urn:cts:greekLit:tlg0012.tlg001.perseus-grc1">
+        <word id="1" form="μῆνιν" lemma="μῆνις" postag="n-s---fa-" relation="OBJ" cite="urn:cts:greekLit:tlg0012.tlg001:1.1" head="2" gloss="wrath, anger" />
+        <word id="2" form="ἄειδε" lemma="ἀείδω" postag="v2spma---" relation="PRED_CO" cite="urn:cts:greekLit:tlg0012.tlg001:1.1" head="32" gloss="sing" />
+        <word id="3" form="θεὰ" lemma="θεά" postag="n-s---fv-" relation="ExD" cite="urn:cts:greekLit:tlg0012.tlg001:1.1" head="2" notes="θεά, the Μοῦσα of Od. 1.1, who tells the poet the history which he has to relate; see 2.484-92, and compare Od. 22.347 αὐτοδίδακτος δ' εἰμί, θεὸς δέ μοι ἐν φρεσὶν οἰμας παντοίας ἐνέφυσεν, and Od. 8.44, 64, 488 ἢ σέ γε Μοῦσ' ἐδίδαξε, Διὸς πάϊς, ἢ σέ γ' Ἀπόλλων." gloss="goddess" />
+        <word id="4" form="Πηληϊάδεω" lemma="Πηληιάδης" postag="n-s---mg-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.1" head="5" notes="Πηληϊάδεω, originally no doubt Πηληϊάδα'(ο). This is one of a class of patronymics formed with a double suffix, the adjectival -ιο- and the purely patronymic -αδη-ς: while the commoner form Πηλε-ίδη-ς has only one. Cf. 2.566." />
+        <word id="5" form="Ἀχιλῆος" lemma="Ἀχιλλεύς" postag="n-s---mg-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.1" head="1" gloss="Achilles" />
+        <word id="6" form="οὐλομένην" lemma="οὐλόμενος" postag="a-s---fa-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.2" head="1" notes="οὐλομένην, accursed; it bears the same relation to the curse ὄλοιο as ὀνήμενος (Od. 2.33) to the blessing ὄναιο, and means 'that of which we say ὄλοιο.' It is best regarded as a purely metrical variant of ὀλόμενος, which occurs in the same sense in Trag. (Eur. Hel. 231 , Eur. Phoen. 1029, Eur. Or. 1363, Eur. Herc. 1061); see Schulze Qu. Ep. pp. 192 ff." gloss="destructive, baneful" />
+        <word id="7" form="," lemma="," postag="u--------" relation="AuxX" cite="" head="23"/>
+        <word id="8" form="ἣ" lemma="ὅς" postag="p-s---fn-" relation="SBJ" cite="urn:cts:greekLit:tlg0012.tlg001:1.2" head="23" gloss="who, which, that" />
+        <word id="9" form="μυρί'" lemma="μυρίος" postag="a-p---na-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.2" head="11" notes="μυρία, countless; in its later sense, 10,000, the word is accented μύριοι." gloss="countless" />
+        <word id="10" form="Ἀχαιοῖς" lemma="Ἀχαιός" postag="a-p---md-" relation="OBJ" cite="urn:cts:greekLit:tlg0012.tlg001:1.2" head="12" gloss="Achaian" />
+        <word id="11" form="ἄλγε'" lemma="ἄλγος" postag="n-p---na-" relation="OBJ" cite="urn:cts:greekLit:tlg0012.tlg001:1.2" head="12" gloss="pain" />
+        <word id="12" form="ἔθηκε" lemma="τίθημι" postag="v3saia---" relation="ATR_CO" cite="urn:cts:greekLit:tlg0012.tlg001:1.2" head="23" gloss="put" />
+        <word id="13" form="," lemma="," postag="u--------" relation="AuxX" cite="" head="23"/>
+        <word id="14" form="πολλὰς" lemma="πολύς" postag="a-p---fa-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.3" head="17" gloss="many" />
+        <word id="15" form="δ'" lemma="δέ" postag="g--------" relation="AuxY" cite="urn:cts:greekLit:tlg0012.tlg001:1.3" head="23" gloss="and, but" />
+        <word id="16" form="ἰφθίμους" lemma="ἴφθιμος" postag="a-p---ma-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.3" head="17" notes="ἴφθιμος here, as in 24 other places (Knös), does not admit an initial ϝ and never requires it. Thus connexion with ϝίς, ϝίφιος is impossible, in spite of the nearness of sense. For a suggested etymology see Collitz in AJP. viii. 214-7. The feminine ἰφθίμη is also found, but only applied to women-e.g. 19.116." gloss="stout, strong, stalwart" />
+        <word id="17" form="ψυχὰς" lemma="ψυχή" postag="n-p---fa-" relation="OBJ" cite="urn:cts:greekLit:tlg0012.tlg001:1.3" head="19" gloss="breath" />
+        <word id="18" form="Ἄϊδι" lemma="Αἵδης" postag="n-s---md-" relation="OBJ" cite="urn:cts:greekLit:tlg0012.tlg001:1.3" head="19" notes="Ἄϊδι, a metaplastic dative of Ἀΐδης, which in H. always means the god, not his realm-with the exception, apparently, of 23.244."/>
+        <word id="19" form="προΐαψεν" lemma="προιάπτω" postag="v3saia---" relation="ATR_CO" cite="urn:cts:greekLit:tlg0012.tlg001:1.3" head="23" notes="προΐαψε: προ implies 'forth on their way,' as in προπέμπειν, προιέναι (195, 442, etc.). ἰαπ- = iac-, so that προΐαψεν = pro-iec-it exactly." gloss="to send forward, to send untimely" />
+        <word id="20" form="ἡρώων" lemma="ἥρως" postag="n-p---mg-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.4" head="17" gloss="hero" />
+        <word id="21" form="," lemma="," postag="u--------" relation="AuxX" cite="" head="23"/>
+        <word id="22" form="αὐτοὺς" lemma="αὐτός" postag="a-p---ma-" relation="OBJ" cite="urn:cts:greekLit:tlg0012.tlg001:1.4" head="25" notes="αὐτούς: the body is to Homer the real self, the ψυχή is a mere shadow; cf. 23.65, where the soul of Patroklos is πάντ' αὐτῶι εἰκυῖα, like the real man." gloss="self , same, s/he/it" />
+        <word id="23" form="δὲ" lemma="δέ" postag="g--------" relation="COORD" cite="urn:cts:greekLit:tlg0012.tlg001:1.4" head="1" gloss="and, but" />
+        <word id="24" form="ἑλώρια" lemma="ἑλώριον" postag="n-p---na-" relation="OCOMP" cite="urn:cts:greekLit:tlg0012.tlg001:1.4" head="25" gloss="booty, spoil, prey" />
+        <word id="25" form="τεῦχε" lemma="τεύχω" postag="v3siia---" relation="ATR_CO" cite="urn:cts:greekLit:tlg0012.tlg001:1.4" head="23" gloss="build"/>
+        <word id="26" form="κύνεσσιν" lemma="κύων" postag="n-p---md-" relation="OBJ_CO" cite="urn:cts:greekLit:tlg0012.tlg001:1.4" head="28" gloss="dog" />
+        <word id="27" form="οἰωνοῖσί" lemma="οἰωνός" postag="n-p---md-" relation="OBJ_CO" cite="urn:cts:greekLit:tlg0012.tlg001:1.5" head="28" gloss="a large bird, bird of prey" />
+        <word id="28" form="τε" lemma="τε" postag="g--------" relation="COORD" cite="urn:cts:greekLit:tlg0012.tlg001:1.5" head="25" gloss="and" />
+        <word id="29" form="πᾶσι" lemma="πᾶς" postag="a-p---md-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.5" head="27" notes="πᾶσι, i.e. all that chose to come: a perfectly natural expression. The reading δαῖτα ascribed to Zen. is not mentioned in the scholia, which merely say that he athetized 4-5. The only authority for the statement is Athenaeus (i. p. 12), on whom no reliance can be placed. But the reading is in itself vigorous and poetical. In fact the metaphor is so natural that we cannot even argue with confidence that Aischylos had δαῖτα before him when he wrote (Supp. 800) κυσὶν δ' ἔπειθ' ἕλωρα κἀπιχωρίοις | ὄρνισι δεῖπνον οὐκ ἀναίνομαι πελεῖν: or Eur. Hec. 1077 σφακτὰν κυσί τε φονίαν δαῖτ' ἀνήμερον, Ion 505 πτανοῖς ἐξώρισε θοίναν θηρσί τε φοινίαν δαῖτα (Soph. is neutral, Aj. 830 ῥιφθῶ κυσὶν πρόβλητος οἰωνοῖς θ' ἕλωρ). In all these cases there is an apparent echo of the present passage, and δαῖτα if a real variant is much older than Zen. The argument against it in Athenaeus (often ascribed, though without ground, to Ar.), that H. never uses δαίς except of human banquets, is not even based on fact, see 24.43. On the whole δαῖτα seems intrinsically a better reading, but we have no right to leave the uniform tradition of the MSS." gloss="all" />
+        <word id="30" form="," lemma="," postag="u--------" relation="AuxX" cite="" head="32"/>
+        <word id="31" form="Διὸς" lemma="Ζεύς" postag="n-s---mg-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.5" head="34" gloss="Zeus" />
+        <word id="32" form="δ'" lemma="δέ" postag="g--------" relation="COORD" cite="urn:cts:greekLit:tlg0012.tlg001:1.5" head="0" gloss="and, but" />
+        <word id="33" form="ἐτελείετο" lemma="τελέω" postag="v3siie---" relation="PRED_CO" cite="urn:cts:greekLit:tlg0012.tlg001:1.5" head="32" gloss="fulfil" />
+        <word id="34" form="βουλή" lemma="βουλή" postag="n-s---fn-" relation="SBJ" cite="urn:cts:greekLit:tlg0012.tlg001:1.5" head="33" gloss="plan, council" />
+        <word id="35" form="," lemma="," postag="u--------" relation="AuxX" cite="" head="41"/>
+        <word id="36" form="ἐξ" lemma="ἐκ" postag="r--------" relation="AuxP" cite="urn:cts:greekLit:tlg0012.tlg001:1.6" head="41" notes="ἐξ οὗ may refer to the preceding line, 'the will of Zeus was being fulfilled from the time when' (so Ar.); or better, to ἄειδε in the first line, 'take up the song from the point when,' as in Od. 8.500 φαῖνε δ' ἀοιδήν, ἔνθεν ἑλών, ὡς οἱ μέν, κτλ. The extraordinary variant διὰ στήτην (ἐρίσαντο) was explained to mean 'on account of a woman' (!)" gloss="+ gen from, out of" />
+        <word id="37" form="οὗ" lemma="ὅς" postag="p-s---mg-" relation="ADV" cite="urn:cts:greekLit:tlg0012.tlg001:1.6" head="36" notes="ἐξ οὗ may refer to the preceding line, 'the will of Zeus was being fulfilled from the time when' (so Ar.); or better, to ἄειδε in the first line, 'take up the song from the point when,' as in Od. 8.500 φαῖνε δ' ἀοιδήν, ἔνθεν ἑλών, ὡς οἱ μέν, κτλ. The extraordinary variant διὰ στήτην (ἐρίσαντο) was explained to mean 'on account of a woman' (!)" gloss="who, which, that" />
+        <word id="38" form="δὴ" lemma="δή" postag="g--------" relation="AuxY" cite="urn:cts:greekLit:tlg0012.tlg001:1.6" head="41" gloss="now" />
+        <word id="39" form="τὰ" lemma="ὁ" postag="l-p---na-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.6" head="40" gloss="the" />
+        <word id="40" form="πρῶτα" lemma="πρῶτος" postag="a-p---na-" relation="ADV" cite="urn:cts:greekLit:tlg0012.tlg001:1.6" head="41" gloss="first" />
+        <word id="41" form="διαστήτην" lemma="διίστημι" postag="v3daia---" relation="ADV" cite="urn:cts:greekLit:tlg0012.tlg001:1.6" head="33" gloss="to set apart, to place separately, separate" />
+        <word id="42" form="ἐρίσαντε" lemma="ἐρίζω" postag="v-dapamn-" relation="ADV" cite="urn:cts:greekLit:tlg0012.tlg001:1.6" head="41" gloss="to strive, wrangle, quarrel" />
+        <word id="43" form="Ἀτρεΐδης" lemma="Ἀτρείδης" postag="n-s---mn-" relation="SBJ_CO" cite="urn:cts:greekLit:tlg0012.tlg001:1.7" head="47"/>
+        <word id="44" form="τε" lemma="τε" postag="g--------" relation="AuxY" cite="urn:cts:greekLit:tlg0012.tlg001:1.7" head="47" gloss="and" />
+        <word id="45" form="ἄναξ" lemma="ἄναξ" postag="n-s---mn-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.7" head="43" gloss="ruler, lord" />
+        <word id="46" form="ἀνδρῶν" lemma="ἀνήρ" postag="n-p---mg-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.7" head="45" gloss="man" />
+        <word id="47" form="καὶ" lemma="καί" postag="c--------" relation="COORD" cite="urn:cts:greekLit:tlg0012.tlg001:1.7" head="41" gloss="and" />
+        <word id="48" form="δῖος" lemma="δῖος" postag="a-s---mn-" relation="ATR" cite="urn:cts:greekLit:tlg0012.tlg001:1.7" head="49" gloss="divine" />
+        <word id="49" form="Ἀχιλλεύς" lemma="Ἀχιλλεύς" postag="n-s---mn-" relation="SBJ_CO" cite="urn:cts:greekLit:tlg0012.tlg001:1.7" head="47" gloss="Achilles" />
+        <word id="50" form="." lemma="." postag="u--------" relation="AuxK" cite="" head="0"/>
+      </sentence>
+ </treebank>
+`;
