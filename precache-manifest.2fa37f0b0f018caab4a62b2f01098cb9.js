4eb129fbb4abbefe9480b1104a88d2adb4f14ce4
zfletch <zfletch2@gmail.com>
2020-11-16 12:06:43 -0500

    Updates

---
+++ precache-manifest.2fa37f0b0f018caab4a62b2f01098cb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf221eb0f4b807788f21e4a3de34c268",
+    "url": "/commentary-demo/index.html"
+  },
+  {
+    "revision": "507cd1f650272d917bff",
+    "url": "/commentary-demo/static/css/main.89218f36.chunk.css"
+  },
+  {
+    "revision": "8c6136569dff5d4ca536",
+    "url": "/commentary-demo/static/js/2.ebc5a30e.chunk.js"
+  },
+  {
+    "revision": "507cd1f650272d917bff",
+    "url": "/commentary-demo/static/js/main.6521c0b4.chunk.js"
+  },
+  {
+    "revision": "50107dec73da7d801ba4",
+    "url": "/commentary-demo/static/js/runtime-main.9e5c556d.js"
+  }
+]);
